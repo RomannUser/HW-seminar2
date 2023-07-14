6b4ac4e557cdc3c470701babbdbb3890c3750ebc
Romann <romikusmatveev@mail.ru>
2023-07-14 20:55:37 +0500

    gotovo

--- zadanie3/Program.cs
+++ zadanie3/Program.cs
@@ -3,3 +3,18 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void DayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine("не выходной");
+}
+
+DayOfTheWeek(dayNumber);
